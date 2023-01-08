914a478b31647216863fb6c72eec4c733e8cc6b1
ylmzoguzhan <80750095+ylmzoguzhan@users.noreply.github.com>
2023-01-08 10:22:29 +0300

    Veri Silme

---
+++ VeriSilme/Program.cs
@@ -0,0 +1,16 @@
+﻿NorthwindContext.NorthwindDbContext northwindDbContext = new();
+TestDB.TestDbContext testDbContext = new();
+
+//Takip edilen nesneler için
+var entity = testDbContext.Products.FirstOrDefault(op => op.ID == 2);
+var result = testDbContext.Products.Remove(entity);
+
+//Takip edilmeyen nesneler için 
+TestDB.Customer customer = new()
+{
+     ID = 1
+};
+//var entry = testDbContext.Customers.Entry(customer);
+//entry.State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+testDbContext.Customers.Remove(customer);
+testDbContext.SaveChanges();
